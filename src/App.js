522adad97774777e9282e0a13d4761d75f48790f
Harshamasina <18uec021@lnmiit.ac.in>
2023-01-31 17:02:10 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
-import NavBar from './Components/Navbar/NavBar.js';
+import NavBar from './Components/Navbar/Navbar.js';
 import Footer from './Components/Footer/Footer.js';
-import GoToTopButton from './Components/Body/GoToTopBtn.js';
+import GoToTopButton from './Components/Body/GoToTopBtn';
 
 function App() {
   return (
